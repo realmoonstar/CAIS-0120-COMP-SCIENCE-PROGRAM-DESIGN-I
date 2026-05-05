c6220700d1002d9984c8bab06e75f08a8692c0cf
Moonstar <romerotaisha123@gmail.com>
2026-05-05 13:18:44 -0400

    Add files via upload

---
+++ loginvalidation.java.java
@@ -0,0 +1,52 @@
+﻿public class LoginValidation {
+
+    private String expectedUserName;
+    private String expectedPassword;
+    private int maxAttempts;
+    private int curAttempts;
+
+    public LoginValidation(String initExpectedUsername,
+                           String initExpectedPassword,
+                           int initMaxAttempts) {
+
+        expectedUserName = initExpectedUsername;
+        expectedPassword = initExpectedPassword;
+        maxAttempts = initMaxAttempts;
+        curAttempts = 0;
+    }
+
+    public boolean setParameters(String correctUserName,
+                                 String correctPassword,
+                                 int maxAttempts) {
+
+        if (correctUserName == null || correctUserName.equals("")) return false;
+        if (correctPassword == null || correctPassword.equals("")) return false;
+        if (maxAttempts <= 0) return false;
+
+        this.expectedUserName = correctUserName;
+        this.expectedPassword = correctPassword;
+        this.maxAttempts = maxAttempts;
+        this.curAttempts = 0;
+
+        return true;
+    }
+
+    public int validateLogin(String username, String password) {
+
+        if (curAttempts >= maxAttempts) {
+            return 4; // attempts exceeded
+        }
+
+        curAttempts++;
+
+        boolean userOK = username.equals(expectedUserName);
+        boolean passOK = password.equals(expectedPassword);
+
+        if (userOK && passOK) return 0;
+        if (!userOK && passOK) return 1;
+        if (userOK && !passOK) return 2;
+        if (!userOK && !passOK) return 3;
+
+        return 3;
+    }
+}
