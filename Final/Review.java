6e1932faa433502eacd3bf8cb61dc8ff67a70726
Moonstar <romerotaisha123@gmail.com>
2026-05-06 12:15:17 -0400

    Update Review.java

--- Final/Review.java
+++ Final/Review.java
@@ -6,68 +6,68 @@ Person  person1 = new Person(“Mark”, ‘m’, 33, 22);
 Person  person2  = new Student(“Fred”,’m’,34, 27,”67777”);  
 Student student2 = null; 
      
-student2  = (Student)person2;
+student2  = (Student)person2;//student cannot inherit Person class
 person2 = student1;
 student1.setEyeColor("green");
 person2.setHairColor(“blond”);
         	System.out.println("The eye color of the student is:" + student1.getEyeColor());
 System.out.println("The eye color of the student is:" + person2.getEyeColor());
 System.out.println("The id number of the student is:" + student2.getStudentId());
-              student2 = (Student)person1;
+              student2 = (Student)person1;//student cannot inherit Person class
         	student2.setAge(20);
         	student1.growOlder(2);
         	System.out.println("The student age is: " + student1.getAge()); 
 
 
-//2 – What will be the output of the following code:
+//2 – What will be the output of the following code: Hello World , mouse, none class C message void?
 Class A
 {
-	public A()
+		public A()
+		{
+	}
+	
+	public String info()
 	{
+	return “world”; 
+	}
+	
+	public void Message()
+	{
+		System.out.println(“Hello ” + info());
+	}
+	
+	public static void main(String[] args)
+	{
+		A a = new B();
+		B  b = new C();    
+		A  c = new A();
+		a.Message();
+		b.Message();
+		c.Message();	
+	} 
 }
-
-public String info()
-{
-return “world”; 
-}
-
-public void Message()
-{
-	System.out.println(“Hello ” + info());
-}
-
-public static void main(String[] args)
-{
-	A a = new B();
-	B  b = new C();    
-	A  c = new A();
-	a.Message();
-	b.Message();
-c.Message();	
-} 
-}
- Class B extends A
+ Class B extends A 
 {
 	Public B()
 	{
 	    super();
-}
-public String info()
-{
-return “mouse”; 
-}
-}
-
-Class C extends B
-{
-	Public C()
+	}
+	public String info()
 	{
-	    super();
-}
-Public void Message()
-{
-		System.out.println(“Goodbye World”);
-}
+	return “mouse”; 
+	}
+	}
+	
+	Class C extends B
+	{
+		Public C()
+		{
+		    super();
+	}
+	Public void Message()
+	{
+			System.out.println(“Goodbye World”);
+	}
 }
 
 
@@ -97,10 +97,95 @@ public void growOlder(int years)
 } 
 }
 
-//Write a Student class that extends the Person class by adding an id property and a number of courses taken property, also change the functionality of the growOlder behavior to increment the age by twice the amount of years if the student is taking more than ten courses at a time.
+//Write a Student class that extends the Person class by adding an id property and a number of courses taken property,
+//also change the functionality of the growOlder behavior to increment the age by twice the amount of years if the student is taking more than ten courses at a time.
 //Do this in two different ways, first do it by using Containment/Composition and then do it by using Inheritance.
-//4 – The constructor of a Balloon class takes as parameter a string specifying the color of the balloon. Write some code to declare and allocate an array of ten balloons and initialize all the balloons to be white.
-//5 – Create a two dimensional array of integers, fill it with random values and write some code that finds the maximum even element in the array.  
+Class Student extends Class Person
+{
+	private int idNum;
+	private int courses;
+	
+	Public Student(String name, int age, int idNum, int courseNum)
+	{
+		super(String name, int age);
+		this.idNum = idNum;
+		this.courseNum = courseNum;
+	}
+
+	public int getidNum()
+	{
+		return idNum;
+	}
+
+	public void setidNum(int idNum)
+	{
+		this.idNum = idNum;
+	}
+	public int getcourseNum()
+	{
+		return courseNum;
+	}
+
+	public void setcourseNum(int courseNum)
+	{
+		this.courseNum = courseNum;
+	}
+	public void growOlder(int years)
+	{
+		if courseNum > 10:
+			age += 2*years;
+	} 
+}
+
+
+//4 – The constructor of a Balloon class takes as parameter a string specifying the color of the balloon. 
+//Write some code to declare and allocate an array of ten balloons and initialize all the balloons to be white.
+//
+Class Balloon
+{
+	private String color;
+    public Balloon(String color, int amount)
+	{
+		this.color = color;
+		this.amount = amount;
+	}
+
+	public int getColor()
+	{
+	   return color;
+	}
+	public void setColor(String color)
+	{
+	   this.color = color;
+	}
+	public int getAmount()
+	{
+	   return amount;
+	}
+	public void setAmount(int amount)
+	{
+	   this.amount = amount;
+	}
+
+	public void changeColor(String white)
+	{
+		for 10 in amount://every array set of 10 balloons set the color white
+			color = white;
+	} 
+}
+
+
+//5 – Create a two dimensional array of integers, fill it with random values and write some code that finds the maximum even element in the array. 
+import random from randit
+Public class Numbers
+{
+	array = array[]
+	private int randomValues;
+	randomValues = array[].append(randint.random)
+	
+}
+
+
 //6- 
 //Part 1:
 //The following is a simple Car class:
